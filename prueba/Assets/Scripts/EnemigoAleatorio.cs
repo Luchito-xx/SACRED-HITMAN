ceffbf140aed719bc1d8a69c6ada9a7b119cb848
Fernandez Actis Luciano Agustin <la.fernandez@etec.local>
2023-08-17 12:34:57 -0300

    enemigo alaeatorio finalizado con aparicion y velocidad alaeatoria

--- prueba/Assets/Scripts/EnemigoAleatorio.cs
+++ prueba/Assets/Scripts/EnemigoAleatorio.cs
@@ -7,30 +7,27 @@ public class EnemigoAleatorio : MonoBehaviour
     
     private Rigidbody2D rb2D;
     private SpriteRenderer mySpriteRenderer;
+    private float tiempo;
 
     [SerializeField] private float velocidadMovimiento;
 
     [SerializeField] private float distancia;
 
     [SerializeField] private LayerMask queEsSuelo;
-
+    
 
     void Start()
     {
         rb2D = GetComponent<Rigidbody2D>();
         mySpriteRenderer = GetComponent<SpriteRenderer>();
+        DatosAleatorio();
+        InvokeRepeating("Girar", 1f, tiempo);
+        InvokeRepeating("VelocidadAleatoria", 1f, 4f);
     }
 
     void Update()
     {
         rb2D.velocity = new Vector2(velocidadMovimiento * transform.right.x, rb2D.velocity.y);
-        
-        RaycastHit2D informacionSuelo = Physics2D.Raycast(transform.position, transform.right, distancia, queEsSuelo);
-
-        if (informacionSuelo)
-        {
-            Girar();
-        }
     }
 
     private void Girar()
@@ -38,9 +35,22 @@ public class EnemigoAleatorio : MonoBehaviour
         transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
     }
 
+
+
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + transform.right * distancia);
+    }
+
+    private void DatosAleatorio()
+    {
+        tiempo = Random.Range(3f , 7f);
+            
+    }
+
+    private void VelocidadAleatoria()
+    {
+        velocidadMovimiento = Random.Range(3f, 8f);
     }
 }
